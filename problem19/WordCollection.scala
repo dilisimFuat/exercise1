284448c778debb1cd7cbedf75cb93659a5e9af35
Fuat <fuat@dilisim.com>
2016-06-08 08:45:24 -0700

    added problem 19

---
+++ problem19/WordCollection.scala
@@ -0,0 +1,23 @@
+val htmlrdd = sc.textFile("/loudacre/kb/*")
+val rdd = htmlrdd.mapPartitionsWithIndex{
+      (files, iterator) => {
+      //println("Partition -> "+files)
+      val myList = iterator
+.toList
+	  val firstrow = myList(0)
+      //myList.map(x => x + " -> "+files).iterator
+	   myList.map{x => 
+		{
+			if(x.equals(firstrow))
+				//(firstrow, 1)
+				1
+			else
+				//(firstrow, 0)
+				0
+		}
+	   }.iterator
+
+      }
+      }
+#rdd.sum()
+rdd.saveAsTextFile("/loudacre/mapPartittion2")
