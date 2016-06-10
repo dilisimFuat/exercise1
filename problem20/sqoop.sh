46810eea9008f89d3b9eaa01ab43b884f96b30d7
Fuat <fuat@dilisim.com>
2016-06-10 00:25:58 -0700

    added some comments

--- problem20/sqoop.sh
+++ problem20/sqoop.sh
@@ -1,4 +1,5 @@
-#Creates the problem20 with one command.
+#Creates a hive table, the problem20, from an existing mysql table, columns,
+#based on given parameters, such as culumns and where clause  with one command.
 sqoop import \
 --connect jdbc:mysql://localhost/information_schema \
 --username training -P \
