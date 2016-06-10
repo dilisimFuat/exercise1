94cebe9a4ac3999b8677487fca26f5fb48daca35
Fuat <fuat@dilisim.com>
2016-06-10 03:18:59 -0700

    added comments in the head of each files

--- problem21/AvgBasedOnColumn.py
+++ problem21/AvgBasedOnColumn.py
@@ -1,3 +1,8 @@
+""" Finds average of each column 4(latitiude) based on column 2 (device model)
+	EXAMPLE INPUT
+	2014-03-15:10:10:20,Sorrento,8cc3b47e-bd01-4482-b500-28f2342679af,33.6894754264,-117.543308253
+	2014-03-15:10:10:20,MeeToo,ef8c7564-0a1a-4650-a655-c8bbd5f8f943,37.4321088904,-121.485029632
+"""
 devicestatus_etl = sc.textFile("/loudacre/devicestatus_etl") \
     .map(lambda line: line.split(',')) \
     .map(lambda line: (line[1], (float(line[3]), 1))) \
