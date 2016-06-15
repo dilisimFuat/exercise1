df12a118c13a132ed8af4d6d86164fc0e9c11dd9
Fuat <fuat@dilisim.com>
2016-06-15 06:01:08 -0700

    added spark submit version of files

---
+++ problem16/WC_Count/src/main/scala/solution/WC_Count.scala
@@ -0,0 +1,29 @@
+package solution16
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object WC_Count {
+   def main(args: Array[String]) {
+     if (args.length < 1) {
+       System.err.println("Usage: WC_EachFile <in file> <out file>")
+       System.exit(1)
+     }
+
+     val sc = new SparkContext()
+     
+     
+
+     val totals = sc.wholeTextFiles(args(0))
+			.map(files => (files._1.split("/").last, files._2))
+			.flatMapValues(content => content.split("\\s+"))
+			.map(word => (word, 1)).reduceByKey(_+_)
+			.map(line => line._1._1+"|"+line._1._2+"|"+line._2)
+
+	 totals.saveAsTextFile(args(1))
+
+     sc.stop
+   }
+ }
+
--- problem16/WC_EachFile.py
+++ problem16/WC_EachFile.py
@@ -19,14 +19,26 @@ totals = sc.parallelize([])
 for i in htmlrdd.collect():
     totals = totals.union(wcount(i))
 '''
-from bs4 import BeautifulSoup
-totals = sc.wholeTextFiles("/loudacre/kb*") \
-    	.map(lambda (fname, content): (str(fname.split("/")[-1]), content)) \
-    	.map(lambda (fname,line):  (fname,BeautifulSoup(line, 'html.parser').get_text().strip())) \
-    	.flatMapValues(lambda x: x.split()) \
-    	.map(lambda x: (x, 1)) \
-    	.reduceByKey(lambda x, y: x + y) \
-     	.map(lambda ((fname,word), count): fname+"|"+word+"|"+`count`).take(10)
 
-totals.saveAsTextFile("/user/training/problem16/")
+#To Run spark-submit WC_EachFile.py <in-file> <out-file>
+from bs4 import BeautifulSoup
+from pyspark import SparkContext
+import sys
+
+if __name__ == "__main__":
+	if(len(sys.argv) < 3):
+		print("Usage: This file <input_file> <output_file>", file=sys.stderr)
+        exit(-1)
+
+	sc = SparkContext()
+	totals = sc.wholeTextFiles(sys.argv[1]) \
+			.map(lambda (fname, content): (str(fname.split("/")[-1]), content)) \
+			.map(lambda (fname,line):  (fname,BeautifulSoup(line, 'html.parser').get_text().strip())) \
+			.flatMapValues(lambda x: x.split()) \
+			.map(lambda x: (x, 1)) \
+			.reduceByKey(lambda x, y: x + y) \
+		 	.map(lambda ((fname,word), count): fname+"|"+word+"|"+`count`).take(10)
+
+	totals.saveAsTextFile(sys.argv[2])
+	sc.stop()
 
